bba4d30d872807d13d053c6646ce994ba08124c4
Kashish Thakur <abhimanhas2132@gmail.com>
2026-05-22 18:31:11 +0530

    demo 5

---
+++ demo5/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <p className="flex items-center h-screen justify-center pt-4 text-red-500 text-3xl">hello</p>
+      <h1 className="text-6xl h-screen m-auto py-7">kashish</h1>
+    </>
+
+  );
+}
